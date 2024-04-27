01749935e859cc0d8e4b42ebde6449223ba403a1
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2024-04-27 22:48:33 +0530

    Add files via upload

---
+++ RPC/server.py
@@ -0,0 +1,16 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+
+server = SimpleXMLRPCServer(("localhost",8000))
+print("Server listening on port 8000...")
+
+
+server.register_function(factorial,"calculate_factorial")
+server.serve_forever()
